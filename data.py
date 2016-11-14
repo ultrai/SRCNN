c1474eeb333fc3f9ce422a7e1112fe6d0cd6529d
ultrai <phani.moresmiles@gmail.com>
2016-11-14 17:37:21 +0530

    Add files via upload

--- data.py
+++ data.py
@@ -6,12 +6,16 @@ from sklearn.feature_extraction import image
 type = ".tif"
 path1 = "Images for Dictionaries and Mapping leraning/"
 path2 = "For synthetic experiments/"
+sz_L = (224,448)
+sz_H = (448,896)
 for x in range(1, 11):#<---- as indexing images start from 1
 	#print(x)
 	im = np.array(Image.open(path1 + "LL" + str(x) + type)) 
 	im2 = im[:,range(0,im.shape[1],2)]   #<-removal of intermediate A-scans
-	im = scipy.misc.imresize(im2,np.shape(im)) #<-----  Interpolation to High resolution
+	im2 = im2[range(0,im.shape[0],2),:]   #<-removal of intermediate A-scans
+	im = scipy.misc.imresize(im2,sz_L) #<-----  Interpolation to High resolution
 	hh = np.array(Image.open(path1 + "HH" + str(x) + type)) 
+	hh = scipy.misc.imresize(hh,sz_H) #<-----  Interpolation to High resolution
 	if x==1:
 	   LL = np.ndarray.reshape(im,(1,1,im.shape[0],im.shape[1]))
 	   HH = np.ndarray.reshape(hh,(1,1,hh.shape[0],hh.shape[1]))
@@ -21,8 +25,8 @@ for x in range(1, 11):#<---- as indexing images start from 1
 	   LL = np.concatenate((LL, ll), axis=0)
 	   HH = np.concatenate((HH, hh), axis=0)
 print('Train Data Prepared')   
-LL = LL[:,:,:448,:896]
-HH = HH[:,:,:448,:896]
+#LL = LL[:,:,:448,:896]
+#HH = HH[:,:,:448,:896]
 from skimage.util.shape import view_as_blocks
 #LL  = view_as_blocks(LL.swapaxes(2,3) , block_shape=(1, 1,128,1)).reshape(LL.shape[0]*7,1,448,128).swapaxes(2,3)
 #HH  = view_as_blocks(HH.swapaxes(2,3) , block_shape=(1, 1,128,1)).reshape(HH.shape[0]*7,1,448,128).swapaxes(2,3)
@@ -36,11 +40,11 @@ for x in range(1, 19):
 	if x!=9:
 	   im = np.array(Image.open(path2 +  str(x) +"/test"+type))
 	   hh = np.array(Image.open(path2 +  str(x) +"/average"+type)) 
-	   im2 = im[:,range(0,im.shape[1],2)]   #<-removal of intermediate A-scans
-	   ll = scipy.misc.imresize(im2,np.shape(im)) #<-----  Interpolation to High resolution
-	   ll = scipy.misc.imresize(im2,np.shape(im)) #<-----  Interpolation to High resolution
-	   ll = np.ndarray.reshape(ll,(1,1,ll.shape[0],ll.shape[1]))
-	   if x==1:
+           im2 = im[:,range(0,im.shape[1],2)]   #<-removal of intermediate A-scans
+	   im2 = im2[range(0,im.shape[0],2),:]   #<-removal of intermediate A-scans
+	   im = scipy.misc.imresize(im2,sz_L) #<-----  Interpolation to High resolution
+           hh = scipy.misc.imresize(hh,sz_H) #<-----  Interpolation to High resolution
+           if x==1:
 	  	LL = np.ndarray.reshape(im,(1,1,im.shape[0],im.shape[1]))
 		HH = np.ndarray.reshape(hh,(1,1,hh.shape[0],hh.shape[1]))
 	   else:
@@ -49,8 +53,8 @@ for x in range(1, 19):
  	        LL = np.concatenate((LL, ll), axis=0)
                 HH = np.concatenate((HH, hh), axis=0)
 print('Test Data Prepared')
-LL = LL[:,:,:448,:896]
-HH = HH[:,:,:448,:896]
+#LL = LL[:,:,:448,:896]
+#HH = HH[:,:,:448,:896]
 #LL  = view_as_blocks(LL.swapaxes(2,3) , block_shape=(1, 1,128,1)).reshape(LL.shape[0]*7,1,448,128).swapaxes(2,3)
 #HH  = view_as_blocks(HH.swapaxes(2,3) , block_shape=(1, 1, 128,1)).reshape(HH.shape[0]*7,1,448,128).swapaxes(2,3)
 import h5py
